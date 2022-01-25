1c0b91344303766b629d45f56d115dfbf82dca37
Quentin Schweitzer <36896812+qschweitzer@users.noreply.github.com>
2022-01-25 08:47:30 +0100

    Update POSHTML5.psm1

--- Modules/POSHTML5.psm1
+++ Modules/POSHTML5.psm1
@@ -774,6 +774,36 @@ Function New-PWFProgressBar{
 "@
   return $output
 }
+Function New-PWFBadge{
+  <#
+  .SYNOPSIS
+  Insert a badge.
+  .DESCRIPTION
+  Insert a badge.
+  .PARAMETER YourText
+  The text to display.
+  .PARAMETER Type
+  Type of badge: Primary, Secondary, Success, Danger, Warning, Info, Light, Dark. Default is Primary.
+  .EXAMPLE
+  New-PWFBadge -YourText "Hello" -Type Success
+  .LINK
+  https://github.com/qschweitzer/Powershell-HTML5-Reporting
+  #>
+  param(
+      [Parameter(Mandatory=$true,Position=0)]
+      [string]$YourText,
+      [Parameter(Mandatory=$false,Position=1)]
+      [ValidateSet("default", "primary", "secondary", "success", "danger", "warning","info","light","dark", IgnoreCase = $false)]
+      [string]$Type,
+      [switch]$PillMode
+  )
+
+  $output = @"
+  <span class='badge$(if($PillMode){" rounded-pill"}) bg-$($Type)'>$($YourText)</span>
+"@
+  return $output
+}
+
 Function New-PWFTable{
   <#
   .SYNOPSIS
