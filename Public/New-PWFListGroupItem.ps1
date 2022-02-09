31f8117c9171612c5a578de31907b1b490d954ad
Quentin Schweitzer <36896812+qschweitzer@users.noreply.github.com>
2022-02-09 12:12:23 +0100

    Update functions

---
+++ Public/New-PWFListGroupItem.ps1
@@ -0,0 +1,49 @@
+﻿Function New-PWFListGroupItem {
+    <#
+.SYNOPSIS
+Create a new ListGroup Item.
+.DESCRIPTION
+Create a new ListGroup Item.
+.PARAMETER ItemContent
+The primary text in the Item.
+.PARAMETER SubHeading
+Add a header title on the item.
+.PARAMETER BadgeContent
+Add a badge and set the text on it.
+.PARAMETER BadgeColorHex
+Select the color of the badge.
+.PARAMETER ContextualColor
+Select the color of the Item.
+.EXAMPLE
+New-PWFListGroupItem -ItemContent "Test 3" -SubHeading "SubHeading 3" -BadgeContent "156"
+.LINK
+https://github.com/qschweitzer/Powershell-HTML5-Reporting
+#>
+  param(
+      [Parameter(Mandatory = $true, Position = 0)]
+      $ItemContent,
+      [Parameter(Mandatory = $false, Position = 1)]
+      $SubHeading,
+      $BadgeContent,
+      [ValidateSet("default", "primary", "secondary", "success", "danger", "warning", "info", "light", "dark", IgnoreCase = $false)]
+      $BadgeColorHex = "primary",
+      [ValidateSet("default", "primary", "secondary", "success", "danger", "warning", "info", "light", "dark", IgnoreCase = $false)]
+      $ContextualColor = "default"
+  )
+
+  $output = @"
+  <li class='list-group-item d-flex justify-content-between align-items-start'>
+    <div class='ms-2 me-auto'>
+      $(if($SubHeading){
+        "<div class='fw-bold'>$SubHeading</div>"
+      })
+      $($ItemContent)
+    </div>
+    $(if($BadgeContent){
+      "<span class='badge bg-$($BadgeColorHex) rounded-pill'>$BadgeContent</span>"
+    })
+  </li>
+"@
+
+  return $output
+}
