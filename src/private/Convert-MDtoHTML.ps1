2aee437388e0e0262ce0e073b563c37a3090f39e
Quentin (Cybeardox) Schweitzer <quentin.schweitzer@tdi-services.fr>
2025-05-30 15:00:29 +0200

    feat: enhancing convert markdown format

--- src/private/Convert-MDtoHTML.ps1
+++ src/private/Convert-MDtoHTML.ps1
@@ -8,14 +8,13 @@
 
         write-host "Before first : $Text"
         [string]$Output = ($Text | Out-String) `
-            -replace "#{6}\s(([a-zA-Z0-9\s-!]|\n|\r)*)", "<h6>`$1</h6>"`
-            -replace "#{5}\s(([a-zA-Z0-9\s-!]|\n|\r)*)", "<h5>`$1</h5>"`
-            -replace "#{4}\s(([a-zA-Z0-9\s-!]|\n|\r)*)", "<h4>`$1</h4>"`
-            -replace "#{3}\s(([a-zA-Z0-9\s-!]|\n|\r)*)", "<h3>`$1</h3>"`
-            -replace "#{2}\s(([a-zA-Z0-9\s-!]|\n|\r)*)", "<h2>`$1</h2>"`
-            -replace "#{1}\s(([a-zA-Z0-9\s-!]|\n|\r)*)", "<h1>`$1</h1>"`
-            -replace "\[(([a-zA-Z0-9\s-!]|\n|\r)*)\]\((([a-zA-Z0-9\s-!]|\n|\r)*)\)", "<a href='`$2'>`$1</a>"`
-            -replace "``{3}((.|\n)*)``{3}", "<pre>`$1</pre>"
+            -replace "#{6}\s(([a-zA-Z0-9\/\s-!]|\n|\r)*)", "<h6>`$1</h6>"`
+            -replace "#{5}\s(([a-zA-Z0-9\/\s-!]|\n|\r)*)", "<h5>`$1</h5>"`
+            -replace "#{4}\s(([a-zA-Z0-9\/\s-!]|\n|\r)*)", "<h4>`$1</h4>"`
+            -replace "#{3}\s(([a-zA-Z0-9\/\s-!]|\n|\r)*)", "<h3>`$1</h3>"`
+            -replace "#{2}\s(([a-zA-Z0-9\/\s-!]|\n|\r)*)", "<h2>`$1</h2>"`
+            -replace "#{1}\s(([a-zA-Z0-9\/\s-!]|\n|\r)*)", "<h1>`$1</h1>"`
+            -replace "\[(([a-zA-Z0-9\/\s-!]|\n|\r)*)\]\((([a-zA-Z0-9\/\s-!]|\n|\r)*)\)", "<a href='`$2'>`$1</a>"
         write-host "After first : $Output"
 
         if ($Output.trim() -ne $text.trim()) {
@@ -24,9 +23,11 @@
 
         [string]$Output = ($(if($nochange){$Text | Out-String}else{$Output | Out-String})) `
             -replace "^> (.*)", "<blockquote>`$1</blockquote>" `
-            -replace "\*{2}(([a-zA-Z0-9\s-!]|\n|\r)*)\*{2}", "<strong>`$1</strong>" `
-            -replace "_{2}(([a-zA-Z0-9\s-!]|\n|\r)*)_{2}", "<strong>`$1</strong>" `
-            -replace "==(([a-zA-Z0-9\s-!]|\n|\r)*)==", "<mark>`$1</mark>"`
+            -replace "\*{2}(([a-zA-Z0-9\/\s-!]|\n|\r)*)\*{2}", "<strong>`$1</strong>" `
+            -replace "_{2}(([a-zA-Z0-9\/\s-!]|\n|\r)*)_{2}", "<strong>`$1</strong>" `
+            -replace "==(([a-zA-Z0-9\/\s-!]|\n|\r)*)==", "<mark>`$1</mark>"`
+            -replace "``{1,3}(.*)$", "<pre>`$1"`
+            -replace "^``{1,3}", "</pre>`$1"`
             -replace "\s``(.*)``\s", "<code>`$1</code>"
         Write-Host "After second : $output"
     }
